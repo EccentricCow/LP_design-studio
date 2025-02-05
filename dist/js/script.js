424dda0a4e59a6c6cd498e5e1d0e5a9b45464957
Alexei Derbin <derbinalexei@yandex.ru>
2025-02-05 23:59:17 +0700

    improved for phones

--- dist/js/script.js
+++ dist/js/script.js
@@ -192,7 +192,10 @@ $(document).ready(function () {
 
     if (window.matchMedia('(max-device-width: 1022px)').matches) {
         $('.footer__tel')
-            .prepend('<img src=\"../../images/footer/phone-call.png\" alt=\"Иконка телефона\">');
+            .prepend('<svg class="phone-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="width: 18px">\n' +
+                '                    <path fill="#1c1c1c"\n' +
+                '                          d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/>\n' +
+                '                </svg>');
         $('#footer__icon_tel').remove();
 
         let advantagesNumCenter = $('#advantages__num_center');
@@ -230,7 +233,7 @@ $(document).ready(function () {
             $('#burger-menu__bar').fadeOut();
             $('#burger-menu__background').fadeOut();
         });
-        $('#phone-icon').css('cursor', 'cursor-pointer').on('click', function () {
+        $('.phone-icon').css('cursor', 'cursor-pointer').on('click', function () {
             $('#pop-up').addClass('pop-up_opened');
         });
 
@@ -240,6 +243,7 @@ $(document).ready(function () {
 
         portfolioSlider.slick('unslick');
         portfolioSlider = $('.portfolio__inner');
+        portfolioNumCenter.text('02');
 
         portfolioSlider.eq(0).slick({
             infinite: false,
@@ -248,6 +252,7 @@ $(document).ready(function () {
             slidesToShow: 1,
             centerMode: true,
             variableWidth: true,
+            initialSlide: 1,
             prevArrow: '#portfolio__arrow_prev',
             nextArrow: '#portfolio__arrow_next',
         })
